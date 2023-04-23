30ba364c3e7866563833596373766674e103eab0
francmarin98 <francmarinc@gmail.com>
2023-04-23 15:01:22 -0500

    basic of java variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "Hello World Francisco Marin";
+        System.out.println(message);
+
+        System.out.println("message.toUpperCase() = " + message.toUpperCase());
+
+        int number = 10;
+        System.out.println("number = " + number);
+
+        boolean isAdmin = false;
+        System.out.println("isAdmin = " + isAdmin);
+
+        var dynamicVar = 15;
+        System.out.println("dynamicVar = " + dynamicVar);
+
+
+
+    }
+}
